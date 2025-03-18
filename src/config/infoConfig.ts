d12d6c59b240cb9ca8450cf2908aec7f2f249208
xdcoba <xdcoba@outlook.com>
2025-03-18 17:55:57 +0800

    update name and headline

--- src/config/infoConfig.ts
+++ src/config/infoConfig.ts
@@ -3,17 +3,17 @@ export * from './education'
 export * from './career'
 
 // personal info
-export const name = 'Derek Hu'
-export const headline = 'Teacher at Shanghai Soong Ching Ling School.'
+export const name = 'Kai Jin'
+export const headline = 'UX/UI Designer.'
 export const introduction =
-  "Coucou Les Amis! My name is Derek Hu. I'm from Hubei and an alumnus of BUPT, UChicago, and Tsinghua. I’ve been teaching Computer Science at SCLS since 2020."
-export const email = 'tong.hu@scls-sh.org'
-export const githubUsername = 'derekhut'
+  "My name is Kai Jin. I am originally from Shanghai and am an alumnus of Tsinghua University. I've been working in computer media design since 2020."
+export const email = 'xdcoba@outlook.com'
+export const githubUsername = 'xdcoba'
 
 // about page
 export const aboutMeHeadline = 'Who Are You and Why Should I Care?'
 export const aboutParagraphs = [
-  "Coucou Les Amis! My name is Derek Hu. I'm from Hubei and an alumnus of BUPT, UChicago, and Tsinghua. I’ve been teaching Computer Science at SCLS since 2020.",
+  "My name is Kai Jin. I am originally from Shanghai and am an alumnus of Tsinghua University. I've been working in computer media design since 2020.",
   'Currently I am working on a new course called AI Camp, an innovative course where high school students create real-world products and startups while exploring the frontiers of AI.',
   "I started this blog to share the insights I learn every day. Most blogs focus on education in Artificial Intelligence and general computer science, while others share the life lessons I've learned.",
 ]
